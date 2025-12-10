eff446c01215487d70aefd63d4869c81a5857947
Rolind Joric Alanano <160253914+arjayskiezy@users.noreply.github.com>
2025-12-10 16:34:07 +0800

    feat/ Finals requiirement

--- src/Form/UnitInstanceType.php
+++ src/Form/UnitInstanceType.php
@@ -7,6 +7,9 @@ use App\Entity\UnitInstance;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
